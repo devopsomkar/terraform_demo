24f3cf4982c8c56327a97de7710e72a5891db762
omkar subhash wani <vaniomkar5@gmaIl.com>
2026-04-13 12:39:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -63,6 +63,7 @@ resource "aws_autoscaling_group" "ecs_asg" {
   max_size                  = var.max_size
   health_check_type         = "EC2"
   health_check_grace_period = 300
+  protect_from_scale_in     = true
 
   launch_template {
     id      = aws_launch_template.ecs_launch_template.id
